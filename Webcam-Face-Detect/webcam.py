c95667905c9cddc449225d4aea4acd3d8a57988a
Pablo Carranza Velez <pablocarranza@gmail.com>
2015-11-27 15:40:05 -0800

    Read faces from pipe

--- Webcam-Face-Detect/webcam.py
+++ Webcam-Face-Detect/webcam.py
@@ -1,32 +1,51 @@
 import cv2
 import sys
+import os
+import threading
 
-cascPath = sys.argv[1]
-faceCascade = cv2.CascadeClassifier(cascPath)
+pipeFile = "/data/faces"
+faces = []
+lock = threading.Lock()
 
-video_capture = cv2.VideoCapture(0)
-print(video_capture)
+def toInt(s):
+    return int(s)
+
+def parseFace(face):
+    return map(toInt, face.split(','))
+
+def pipeReader():
+    while True:
+        with open(pipeFile) as openPipe:
+            for line in openPipe:
+                f = map(parseFace, line.split(';'))
+                lock.acquire()
+                faces = f
+                lock.release()
+
+def getFacesFromPipe():
+    lock.acquire()
+    f = faces
+    lock.release()
+    return f
+
+t = threading.Thread(target=pipeReader)
+t.start()
+
+video_capture = cv2.VideoCapture(1)
+cv2.namedWindow("Video", cv2.WND_PROP_FULLSCREEN)          
+cv2.setWindowProperty("Video", cv2.WND_PROP_FULLSCREEN, cv2.cv.CV_WINDOW_FULLSCREEN)
 while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
-    print(ret)
-    print(frame)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
+    faces = getFacesFromPipe()
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     # Display the resulting frame
-    cv2.imshow('Video', frame)
+    cv2.imshow("Video", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- start.sh
+++ start.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+set -e
 
 # Disable DPMS / Screen blanking
 xset -dpms
 xset s off
 xset s noblank
 
+
+if [ ! -p /data/faces ]; then
+	mkfifo /data/faces
+fi
+
+mount -t devtmpfs /dev
+modprobe vv4l2loopback devices=2
+ffmpeg -f video4linux2 -s 800x600 -i /dev/video0 -codec copy -f v4l2 /dev/video1 -codec copy -f v4l2 /dev/video2
+
 cd ./Webcam-Face-Detect
-python2.7 webcam.py haarcascade_frontalface_default.xml
+python2.7 webcam.py
