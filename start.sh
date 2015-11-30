db978c83c773710eefcb373cef4e622c7ce4613f
Pablo Carranza Velez <pablocarranza@gmail.com>
2015-11-30 13:18:33 -0800

    See ffmpeg output

--- start.sh
+++ start.sh
@@ -23,4 +23,16 @@ fi
 supervisorctl -c /etc/supervisor/supervisord.conf start ffmpeg
 
 cd ./Webcam-Face-Detect
-python2.7 webcam.py
+python2.7 webcam.py &
+
+while [ ! -f /var/log/ffmpeg.log ]; do
+	sleep 1
+done
+
+tail -fn 100 /var/log/ffmpeg.log &
+
+while [ ! -f /var/log/ffmpeg_error.log ]; do
+	sleep 1
+done
+
+tail -fn 100 /var/log/ffmpeg_error.log
