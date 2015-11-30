9e7bad9c34cd25b45f6f1162f54b21e069abcc65
Pablo Carranza Velez <pablocarranza@gmail.com>
2015-11-30 10:54:54 -0800

    1280x720

--- start.sh
+++ start.sh
@@ -18,7 +18,7 @@ if [ ! -p /data/video2 ]; then
 	mkfifo /data/video2
 fi
 
-ffmpeg -f video4linux2 -s 800x600 -i /dev/video0 -f avi -y /data/video1 -f avi -y /data/video2 &
+ffmpeg -f video4linux2 -s 1280x720 -i /dev/video0 -f avi -y /data/video1 -f avi -y /data/video2 &
 
 cd ./Webcam-Face-Detect
 python2.7 webcam.py
