1f92c33b79604cc33d6898ccb774add1380da64f
Pablo Carranza Velez <pablocarranza@gmail.com>
2015-11-30 21:11:38 -0800

    very explicit

--- Webcam-Face-Detect/webcam.py
+++ Webcam-Face-Detect/webcam.py
@@ -52,8 +52,13 @@ while True:
         # Draw a rectangle around the faces
         if len(ff) > 0:
             print(ff)
-            for (x, y, w, h) in ff:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            for t in ff:
+                if len(t) == 4:
+                    x = t[0]
+                    y = t[1]
+                    w = t[2]
+                    h = t[3]                    
+                    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         cv2.imshow("Video", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
