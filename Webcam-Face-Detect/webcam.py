c0526387b050cffb8c3562fb89e281bf6e5a80c9
Pablo Carranza Velez <pablocarranza@gmail.com>
2015-11-30 20:23:55 -0800

    wasd

--- Webcam-Face-Detect/webcam.py
+++ Webcam-Face-Detect/webcam.py
@@ -19,9 +19,10 @@ def pipeReader():
         with open(pipeFile) as openPipe:
             for line in openPipe:
                 f = map(parseFace, line.split(';'))
-                lock.acquire()
-                faces = f
-                lock.release()
+                if len(f) > 0:
+                    lock.acquire()
+                    faces = f
+                    lock.release()
 
 def getFacesFromPipe():
     lock.acquire()
@@ -45,8 +46,10 @@ while True:
     # Display the resulting frame
     if ret and len(frame) > 0:
         # Draw a rectangle around the faces
-        for (x, y, w, h) in ff:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        if len(ff) > 0:
+            print(ff)
+            for (x, y, w, h) in ff:
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         cv2.imshow("Video", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
