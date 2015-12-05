7b6152604c388e784087c33a3800242cfea2b20f
Pablo Carranza Velez <pablocarranza@gmail.com>
2015-12-04 16:42:07 -0800

    smaller

--- Webcam-Face-Detect/webcam.py
+++ Webcam-Face-Detect/webcam.py
@@ -100,7 +100,7 @@ while True:
                         h = t[3]                    
                         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
             frame = cv2.flip(frame, 1)
-            cv2.putText(frame, status, (10, 710), cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 1)
+            cv2.putText(frame, status, (10, 710), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255,255,255), 1)
             cv2.imshow("Video", frame)
 
         cv2.waitKey(1)
